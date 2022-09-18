f4736bb00498f946cbc4b65e9a9bcef49c624f86
Diego <df.osorio11@uniandes.edu.co>
2022-09-18 16:52:57 -0500

    Create CodeModelos.R

---
+++ scripts/CodeModelos.R
@@ -0,0 +1,16 @@
+#Código de modelos de ML
+
+#Limpiar el ambiente
+rm(list=ls())
+#Establecer directorios
+#Daniel
+setwd("C:/Users/danie/OneDrive/Escritorio/Uniandes/PEG/Big Data and Machine Learning/BD-ML---PS2/data")
+#Diego
+setwd("C:/Users/Diego/OneDrive/Documents/GitHub/BD-ML---PS2/data")
+#Samuel
+setwd("~/Desktop/Big Data/Repositorios/BD-ML---PS2/data")
+
+# cargar bases para prediccion
+
+# leer la base del archivo .csv
+train_hogares <- read.csv("train_hogares.csv")
